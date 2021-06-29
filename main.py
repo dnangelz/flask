91f206fe93de5599a055d31b5eac510334308581
David Lin <davidlinmonkey@gmail.com>
2021-06-29 13:03:39 +0900

    資料庫連結版

--- main.py
+++ main.py
@@ -1,8 +1,66 @@
-from flask import Flask, render_template
-# 引入必要的模組
-app = Flask(__name__)      #
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, TEXT, select
+
+
+#engine = create_engine("mysql+pymysql://root:Flighter099@localhost:3306/test",encoding="utf-8", echo=True)
+# metadata = MetaData()
+
+# holo = Table('holosql', metadata,
+# Column('Id', TEXT, primary_key=True),
+# Column('本名', TEXT),
+# Column('別號', TEXT),
+# Column('髮色', TEXT),
+# Column('瞳色', TEXT),
+# Column('身高', Integer),
+# Column('年齡', TEXT),
+# Column('生日', TEXT),
+# Column('屬性', TEXT),
+# Column('出身地區', TEXT),
+# Column('初配信', TEXT)
+# )
+# conn = engine.connect()
+app = Flask(__name__)
+db = SQLAlchemy()
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Flighter099@localhost:3306/test"
+app.secret_key='any random string'
+db.init_app(app)
+
 @app.route("/")
 def index():
+
+
     return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def result():
+     if request.method == 'POST':
+         user = request.form.getlist('holo')
+         user=str(user).split(',')[0:4]
+         session['username'] = user
+         return redirect(url_for('test'))
+
+
+@app.route('/second', methods=['GET','POST'])
+def test():
+    holoid=session['username']
+    # s = select([holo]).where(holo.c.Id.like('aki'))
+    # all_users = holo.query.filter_by(Id='aki').all()
+
+    sql = """
+        select * from holosql where Id = "aki"
+    """
+    #
+    # query_data = db.engine.execute(sql_cmd)
+    #
+    # print(db.engine.execute(query_data).fetchone())
+    print(holoid[1])
+    print(db.engine.execute(sql).fetchall())
+    return render_template('second.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,25 @@
 
-        $("input[name=checkbox_4]").change(function () {
+        $("input[name=holo]").change(function () {
             var max = 3;
-            if ($("input[name=checkbox_4]:checked").length == max) {
-                $("input[name=checkbox_4]").attr('disabled', 'disabled');
-                $("input[name=checkbox_4]:checked").removeAttr('disabled');
+            if ($("input[name=holo]:checked").length == max) {
+                $("input[name=holo]").attr('disabled', 'disabled');
+                $("input[name=holo]:checked").removeAttr('disabled');
             } else {
-                $("input[name=checkbox_4]").removeAttr('disabled');
+                $("input[name=holo]").removeAttr('disabled');
             }
         })
+
+        function  search() {
+            var fruit = '';
+
+            var dd = '';
+            var radio = document.getElementsByName("holo");
+            for (var i = 0; i < radio.length; i++) {
+                if (radio[i].checked == true) {
+                    dd = radio[i].value;
+                    fruit = fruit + "," + dd;
+                }
+            }
+
+        }
+
